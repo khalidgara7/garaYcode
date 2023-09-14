7d9edf2f881add4cabef9b652dd97d5fd34dafda
khalidgara7 <khalidgara8@gmail.com>
2023-09-14 23:29:13 +0100

    modification

--- challenge day 1/D8_challeng8/main.c
+++ challenge day 1/D8_challeng8/main.c
@@ -6,6 +6,12 @@ int main()
    // Écrivez un programme C pour afficher la valeur équivalente en octal et en hexadécimal.
    // Entrez un nombre entier et affichez sa valeur équivalente en octal et en hexadécimal.
 
+    nt nombre, octal, hexadecimal;
+    printf("entrer un nombre entier : \n");
+    scanf("%d",&nombre);
+
+    printf("la valeur equivalent le %d en octal est :%o \n",nombre, nombre);  // %o for transform to octal
+    printf("la valeur equivalent le %d en hexadecimal est :%x \n",nombre, nombre); // %x for transform to hexadecimal
 
     return 0;
 }
