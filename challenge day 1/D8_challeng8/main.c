f2c9a00a777d9b171d46f3c80eb7a01dc774afdb
khalid <khalidgara8@gmail.com>
2023-09-15 10:02:50 +0200

    up date

--- challenge day 1/D8_challeng8/main.c
+++ challenge day 1/D8_challeng8/main.c
@@ -6,6 +6,10 @@ int main()
    // Écrivez un programme C pour afficher la valeur équivalente en octal et en hexadécimal.
    // Entrez un nombre entier et affichez sa valeur équivalente en octal et en hexadécimal.
 
+    int numbr;
+     printf("entrer un nombre ");
+     scanf("%d",&numbr);
 
+     printf()
     return 0;
 }
